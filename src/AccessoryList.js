b6320e430b74d63e02945a65e62f45b0ffd38e0e
Peter Munro <peter@petermunro.org>
2018-01-17 18:00:17 +0000

    Retrieve accessories list from GraphQL server

--- src/AccessoryList.js
+++ src/AccessoryList.js
@@ -1,27 +1,30 @@
 import React from "react";
 import Accessory from "./Accessory";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 import "./AccessoryList.css";
 
-const accs = [
-  { id: 1, _type: "Light", name: "Hall Light", brightnessLevel: 100 },
-  {
-    id: 2,
-    _type: "Door",
-    name: "Front Door",
-    isClosed: true,
-    isLocked: true
-  },
-  {
-    id: 3,
-    _type: "Thermostat",
-    name: "Living Room Thermostat",
-    temperature: 72
-  },
-  { id: 4, _type: "Outlet", name: "Kitchen Outlet 1", isOn: true }
-];
+// const accs = [
+//   { id: 1, _type: "Light", name: "Hall Light", brightnessLevel: 100 },
+//   {
+//     id: 2,
+//     _type: "Door",
+//     name: "Front Door",
+//     isClosed: true,
+//     isLocked: true
+//   },
+//   {
+//     id: 3,
+//     _type: "Thermostat",
+//     name: "Living Room Thermostat",
+//     temperature: 72
+//   },
+//   { id: 4, _type: "Outlet", name: "Kitchen Outlet 1", isOn: true }
+// ];
 
-const AccessoryList = () => {
-  const accessoryComps = accs.map(acc => (
+const AccessoryList = ({ data }) => {
+  const accessories = data.accessories || [];
+  const accessoryComps = accessories.map(acc => (
     <Accessory accessory={acc} key={acc.id} />
   ));
   return (
@@ -32,4 +35,27 @@ const AccessoryList = () => {
   );
 };
 
-export default AccessoryList;
+const ACCESSORY_LIST_QUERY = gql`
+  query getAccessories {
+    accessories(offset: 0, limit: 10) {
+      id
+      name
+      __typename
+      ... on Light {
+        brightnessLevel
+      }
+      ... on Door {
+        isClosed
+        isLocked
+      }
+      ... on Thermostat {
+        temperature
+      }
+      ... on Outlet {
+        isOn
+      }
+    }
+  }
+`;
+
+export default graphql(ACCESSORY_LIST_QUERY)(AccessoryList);
--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ import registerServiceWorker from "./registerServiceWorker";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher
+} from "apollo-cache-inmemory";
+
+import introspectionQueryResultData from "./fragmentTypes.json";
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:4001/graphql" }),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({ fragmentMatcher })
 });
 
 ReactDOM.render(
